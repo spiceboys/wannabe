f57d3866ae21abc9d430c7143460cc12b163921c
Gene Pavlovsky <gene.pavlovsky@gmail.com>
2019-03-09 13:37:37 +0100

    StartView: Pre-populate player starting data

--- src/client/StartView.hx
+++ src/client/StartView.hx
@@ -7,8 +7,16 @@ class StartView extends View {
   
   @:attribute var setPlayerDetails:String->House->Void;
   
-  @:state var house:House = null;
-  @:state var name:String = "";
+  static function randomizer<T>(a:Array<T>)
+    return function () return a[Std.random(a.length)];
+  
+  static final randomHouse = randomizer(haxe.EnumTools.createAll(House));
+  static final firstName = randomizer(['Emma', 'Olivia', 'Ava', 'Isabella', 'Sophia', 'Mia', 'Charlotte', 'Amelia', 'Evelyn', 'Abigail', 'Harper', 'Emily', 'Elizabeth', 'Avery', 'Sofia', 'Ella', 'Madison', 'Scarlett', 'Victoria', 'Aria', 'Grace', 'Chloe', 'Camila', 'Penelope', 'Riley', 'Layla', 'Lillian', 'Nora', 'Zoey', 'Mila', 'Aubrey', 'Hannah', 'Lily', 'Addison', 'Eleanor', 'Natalie', 'Luna', 'Savannah', 'Brooklyn', 'Leah', 'Zoe', 'Stella', 'Hazel', 'Ellie', 'Paisley', 'Audrey', 'Skylar', 'Violet']);
+  static final lastName = randomizer(['Smith', 'Johnson', 'Williams', 'Jones', 'Brown', 'Davis', 'Miller', 'Wilson', 'Moore', 'Taylor', 'Anderson', 'Thomas', 'Jackson', 'White', 'Harris', 'Martin', 'Thompson', 'Garcia', 'Martinez', 'Robinson', 'Clark', 'Rodriguez', 'Lewis', 'Lee', 'Walker', 'Hall', 'Allen', 'Young', 'Hernandez', 'King', 'Wright', 'Lopez', 'Hill', 'Scott', 'Green', 'Adams', 'Baker', 'Gonzalez', 'Nelson', 'Carter', 'Mitchell', 'Perez', 'Roberts', 'Turner', 'Phillips', 'Campbell', 'Parker', 'Evans', 'Edwards', 'Collins', 'Stewart', 'Sanchez', 'Morris', 'Rogers', 'Reed', 'Cook', 'Morgan', 'Bell', 'Murphy', 'Bailey', 'Rivera', 'Cooper', 'Richardson', 'Cox', 'Howard', 'Ward', 'Torres', 'Peterson', 'Gray', 'Ramirez', 'James', 'Watson', 'Brooks', 'Kelly', 'Sanders', 'Price', 'Bennett', 'Wood', 'Barnes', 'Ross', 'Henderson', 'Coleman', 'Jenkins', 'Perry', 'Powell', 'Long', 'Patterson', 'Hughes', 'Flores', 'Washington', 'Butler', 'Simmons', 'Foster', 'Gonzales', 'Bryant', 'Alexander', 'Russell', 'Griffin', 'Diaz', 'Hayes']);
+  static final randomName = () -> '${firstName()} ${lastName()}';
+
+  @:state var house:House = randomHouse();
+  @:state var name:String = randomName();
   @:ref var nameInput:js.html.InputElement;
 
   static var SELECTED = css({
--- src/game/Game.hx
+++ src/game/Game.hx
@@ -140,7 +140,7 @@ class GameOf<TPlayer:Player> implements Model {
     return { players: List.fromArray(players) };
   #end
 
-  @:computed @:skipCheck private var pathFinder:pathfinder.Pathfinder = new pathfinder.Pathfinder(this);
+  @:constant @:skipCheck private var pathFinder:pathfinder.Pathfinder = new pathfinder.Pathfinder(this);
 
   @:computed var availableMoves:List<TileInfo> = switch nextUnit {
     case None: null;

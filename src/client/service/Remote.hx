c759299118264643d91fdff1f3fb60abcd7e1736
Gene Pavlovsky <gene.pavlovsky@gmail.com>
2019-03-09 10:48:05 +0100

    Clean up unused code

--- src/client/service/Remote.hx
+++ src/client/service/Remote.hx
@@ -45,40 +45,6 @@ class Remote {
 
 			}
 		).eager();
-
-	}
-	// final player:Player;
-	
-	// public function new(url:String, player, id:RoomId) {
-	// 	super(url);
-	// 	this.player = player;
-	// }
-
-}
-#if false
-class Remote extends WebSocketService {
-	final playerId:String;
-	final when:RemoteSignals;
-
-	public function new(url:String, playerId:String) {
-		super(url);
-		this.playerId = playerId;
-		when = {
-			starting: createSignal("starting")
-		};
 	}
 
-	public function connect():Promise<ServerInfo> {
-		trace('Connecting: url=$url playerId=$playerId');
-		return connectWS()
-			.next(_ -> callAndListen("join", {roomId: 1, playerId: playerId}));
-	}
-
-	public function setReadiness(value:Bool):Void
-		call("setReadiness", value);
 }
-
-typedef RemoteSignals = {
-	final starting:Signal<Noise>;
-}
-#end

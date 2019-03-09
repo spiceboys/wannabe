db01f96bca8ce6337c33a7bbca33bc1f87d1ca91
Juraj Kirchheim <back2dos@googlemail.com>
2019-03-09 02:09:15 +0100

    "specify" protocol

---
+++ src/server/Protocol.hx
@@ -0,0 +1,36 @@
+package server;
+
+import client.service.vo.ServerInfo;
+import game.*;
+import game.Unit;
+
+typedef RoomId = String;
+
+enum ClientMessage {
+  JoinRoom(id:RoomId, player:Player);
+  SetReady(isReady:Bool);
+  GameAction(action:Action);
+  Forfeit;
+}
+
+enum ServerMessage {
+  RoomJoined(players:Array<LobbyPlayer>, serverInfo:ServerInfo);
+  RoomChanged(players:Array<LobbyPlayer>);
+  GameStarted(init:GameInit);
+  GameReaction(reactions:Array<Reaction>);
+}
+
+typedef GameInit = {
+  final width:Int;
+  final tiles:Array<TileKind>;
+  final units:Array<UnitInit>;
+}
+
+typedef UnitInit = UnitStatus & {
+  final id:UnitId;
+  final owner:PlayerId;
+}
+
+typedef LobbyPlayer = Player & {
+  final ready:Bool;
+}
